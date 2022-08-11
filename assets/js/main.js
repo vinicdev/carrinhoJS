a1853289750241a08987ccaf34d959b43c1ec5bb
Vinicius Cardoso <vinic8670@gmail.com>
2022-08-11 17:37:52 -0300

    cards 'dinamicos'

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('salesCart')
+
+function openModal() {
+    modal.style.display = 'block'
+}
+
+function closeModal() {
+    modal.style.display = 'none' 
+}
+
+shoesJson.map((item, index) => {
+    let shoesItem = document.querySelector('.models .card').cloneNode(true)
+    
+    document.querySelector('#contentesCards').appendChild(shoesItem)
+
+    shoesItem.querySelector('.contentsImage img').scr = item.img
+    shoesItem.querySelector('.priceShoes').innerHTML = `R$ ${item.price.toFixed(2)}`
+    shoesItem.querySelector('.nameProduct').innerHTML = item.produto
+
+})
---
+++ assets/js/shoes.js
@@ -0,0 +1,10 @@
+const shoesJson = [
+    {id:1, produto:'Air Jordan Mid Dutch', price:650.00, img:'../img/air-jordan-1-mid-dutch-green-1-400.svg'},
+    {id:2, produto:'Air Jordan High Zoom', price:1.250, img:'../img/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.svg'},
+    {id:3, produto:'Air Jordan Retro High', price:777.00, img:'../img/air-jordan-1-retro-high-court-purple-w-1-400.svg'},
+    {id:4, produto:'Air Jordan Mid Light', price:6.530, img:'../img/air-jordan-1-mid-light-smoke-grey-gs-1-1000.svg'},
+    {id:5, produto:'Air Jordan Mid Black', price:599.00, img:'../img/air-jordan-1-mid-black-noble-red-w-1-1000.svg'},
+    {id:6, produto:'Air Jordan Mid Bright', price:20.050, img:'../img/air-jordan-1-mid-bright-citrus-1-1000 1.svg'},
+    {id:7, produto:'Air Jordan Mid Grey', price:19.99, img:'../img/air-jordan-1-mid-grey-camo-1-1000 1.svg'},
+    {id:8, produto:'Air Jordan Mid Carbon', price:1.010, img:'../img/air-jordan-1-mid-carbon-fiber-1-1000 1.svg'}
+];
