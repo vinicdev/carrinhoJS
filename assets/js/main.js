d61f2b4955c92d1f8ce4c72e60225cdc7d006991
Vinicius Cardoso <vinic8670@gmail.com>
2022-08-22 18:05:42 -0300

    adding new settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,47 @@
 const modal = document.getElementById('salesCart')
-const result = document.querySelector(".listTeste")
-const cart = []
+const result = document.querySelector("#listTable")
 
 function openModal() {
-    modal.style.display = 'block'
+    modal.style.display = 'block';
 }
 
 function closeModal() {
-    modal.style.display = 'none' 
+    modal.style.display = 'none';
 }
 
 shoesJson.map((item, index) => {
-    let shoesItem = document.querySelector('.models .card').cloneNode(true)
+    let shoesItem = document.querySelector('.models .card').cloneNode(true);
     
-    document.querySelector('#contentesCards').appendChild(shoesItem)
+    document.querySelector('#contentesCards').appendChild(shoesItem);
 
     shoesItem.querySelector('.contentsImage').innerHTML = `<img src="${item.img}"/>`
     shoesItem.querySelector('.nameProduct').innerHTML = item.produto
     shoesItem.querySelector('.priceShoes').innerHTML = `R$ ${item.price.toFixed(2)}`
 
-    
+    const sum = 0;
     shoesItem.querySelector('.card a').addEventListener('click', (e) => {
         e.preventDefault()
 
-        const nameShoes = shoesItem.querySelector('.nameProduct').innerText
-        const priceShoes = shoesItem.querySelector('.priceShoes').innerHTML.replace('R$&nbsp;', '')
+        const nameShoes = shoesItem.querySelector('.nameProduct').innerText;
+        const priceShoes = shoesItem.querySelector('.priceShoes').innerHTML.replace('R$&nbsp;', '');
+        const priceShoes2 = item.price;
 
-        console.log(`Nome do produto: ${nameShoes}\nPreço do produto: ${priceShoes}`)  
 
-        guardaItens(nameShoes)
+        console.log(`Nome do produto: ${nameShoes}\nPreço do produto: ${priceShoes}`);
+
+        const node = document.createElement("p");
+        const node2 = document.createElement("small");
+        const name = document.createTextNode(`${nameShoes}`);
+        const price = document.createTextNode(`${priceShoes}`);
+        node.appendChild(name);
+        node2.appendChild(price);
+        result.appendChild(node);
+        result.appendChild(node2);
+
     })
 
 })
 
 
-const guardaItens = (nameShoes) => {
-    const listResult = nameShoes.map((nameShoes) => {
-            return `
-                <li>
-                    <h2>Produto: ${nameShoes}</h2>
-                </li>
-            `;
-        }).join('');
 
-    result.innerHTML = listResult
-}
  
